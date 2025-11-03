dca13dc249ddaa4af96fb711c94097ace7a6a76b
Daniel <danny0988lewis@gmail.com>
2025-11-02 23:08:20 -0300

    Initial commit - ALX React project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import WelcomeMessageMessageMessageMessage from './components/WelcomeMessageMessageMessageMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <WelcomeMessageMessageMessageMessage />
+    </div>
+  );
+}
+
+export default App;
