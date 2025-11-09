e9230f8f54507380772d1efdddeaab8b9542f066
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-09 15:59:20 -0300

    Update Footer.jsx

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,9 +1,17 @@
 function Footer() {
-    return (
-        <footer>
-            <p>© 2023 City Lovers</p>
-        </footer>
-    );
+  return (
+    <footer
+      style={{
+        backgroundColor: "navy",
+        color: "white",
+        fontSize: "12px",
+        textAlign: "center",
+        padding: "10px",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
 }
 
 export default Footer;
