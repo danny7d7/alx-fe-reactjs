a005e75f9162ea38641631c0a313621a2b893076
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-09 15:54:33 -0300

    Update App.jsx

--- alx-react-app/App.jsx
+++ alx-react-app/App.jsx
@@ -1,12 +1,14 @@
-import Header from './Header.jsx';
-import MainContent from './MainContent.jsx';
-import Footer from './Footer.jsx';
+import Header from "./Header.jsx";
+import MainContent from "./MainContent.jsx";
+import Footer from "./Footer.jsx";
+import UserProfile from "./UserProfile.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Danny" age={20} bio="Junior Front-end dev 💻" />
       <Footer />
     </>
   );
