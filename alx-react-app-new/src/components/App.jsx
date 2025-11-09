6e439e0adae29e845558636c439916fc76f2cc7d
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-09 17:26:08 -0300

    Fix import path for Counter component

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -2,7 +2,7 @@ import Header from "./Header.jsx";
 import MainContent from "./MainContent.jsx";
 import Footer from "./Footer.jsx";
 import UserProfile from "./UserProfile.jsx";
-import Counter from "./components/Counter.jsx";  // ← THIS LINE
+import Counter from "Counter.jsx";  // ← THIS LINE
 
 function App() {
   return (
