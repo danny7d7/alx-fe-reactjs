245c225ef39cebaf62863937c272eda16eb887bf
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-09 22:04:21 +0300

    Add files via upload

---
+++ alx-react-app-new/src/UserProfile.jsx
@@ -0,0 +1,13 @@
+function UserProfile({ name, age, bio }) {
+  return (
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{name}</h2>
+      <p>
+        Age: <strong>{age}</strong>
+      </p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
