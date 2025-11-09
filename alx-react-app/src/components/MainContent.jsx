34835fd111514574413d566ba7eb3d6e7bc88513
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-09 15:59:41 -0300

    Update MainContent.jsx

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,9 +1,11 @@
 function MainContent() {
-    return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
+  return (
+    <main
+      style={{ backgroundColor: "lightgray", color: "navy", padding: "10px" }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
 }
 
 export default MainContent;
