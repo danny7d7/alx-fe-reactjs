4c72e4daf424a82cbfc3eed39334fc3b6c7a6e84
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-16 14:22:19 +0300

    Add files via upload

---
+++ recipe-sharing-app/src/components/RecipeStore.jsx
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+// Create the store
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
+
+export default useRecipeStore;
