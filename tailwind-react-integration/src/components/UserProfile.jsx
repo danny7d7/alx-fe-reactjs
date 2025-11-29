87459295499ac43bf8ce86f1374c1ff6989113bd
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-29 06:04:56 -0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8">
+    <div className="user-profile bg-gray-100 p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 hover:shadow-xl">
       <img
         src="https://via.placeholder.com/150"
         className="w-36 h-36 rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 duration-300 transition-transform duration 300 ease-in-out"
