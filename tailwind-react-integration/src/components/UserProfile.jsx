0b200b47134f78af2e00a5e0ad861a7460654641
Daniel Tadesse <danny0988lewis@gmail.com>
2025-11-29 06:03:12 -0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile() {
     <div className="user-profile bg-gray-100 p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8">
       <img
         src="https://via.placeholder.com/150"
-        className="w-36 h-36 rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration 300 ease-in-out"
+        className="w-36 h-36 rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 duration-300 transition-transform duration 300 ease-in-out"
         alt="User"
       />
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
