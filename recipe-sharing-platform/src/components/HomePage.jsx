a9ee897e40639f41148b1cbb3533cd7f862c9329
Daniel Tadesse <danny0988lewis@gmail.com>
2025-12-05 07:12:26 -0300

    Update HomePage.jsx

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -3,7 +3,7 @@ function HomePage({ recipes, onDelete }) {
   import initialData from "./data.json";
   return (
     <div className="px-4 py-10">
-      <h1 className="text-4xl font-bold text-white text-center mb-10 drop-shadow-lg justify-normal">
+      <h1 className="text-4xl font-bold text-white text-center md-10 drop-shadow-lg justify-normal">
         Recipe Collection
       </h1>
       <div className="flex justify-center">
