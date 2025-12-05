1bff27b1593e116f9e138677e256cdd35c742fe2
Daniel Tadesse <danny0988lewis@gmail.com>
2025-12-05 07:07:48 -0300

    Update HomePage.jsx

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,4 +1,6 @@
 function HomePage({ recipes, onDelete }) {
+  import { useState, useEffect } from "react";
+  import initialData from "./data.json";
   return (
     <div className="px-4 py-10">
       <h1 className="text-4xl font-bold text-white text-center mb-10 drop-shadow-lg justify-normal">
